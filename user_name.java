2ef409dfaf2ca0db0d8f6b313a8aba515035c79e
s1270212 <69299802+s1270212@users.noreply.github.com>
2020-08-06 20:28:58 +0900

    user_name
    User name added

---
+++ user_name.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+Scanner sc=new Scanner(System.in);
+String name;
+System.out.println("Who are you?");
+System.out.print("> ");
+name=sc.next();
+System.out.println("Hello, "+name+"!");
